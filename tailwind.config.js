24ee0193c30b68018458bd62140c813e93190015
Michael Nganga <ngangamichael83@gmail.com>
2025-09-29 11:26:03 +0300

    added folder structure , env file and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",   // ✅ important for React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
